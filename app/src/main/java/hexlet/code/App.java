f6174563700cd9a33442d19b7cf03483e841a752
andrg2280 <andrg2280.mail.ru>
2024-04-01 17:52:36 +0300

    added DB STRUCTURE

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,6 +10,10 @@ import java.io.IOException;
 import java.sql.SQLException;
 @Slf4j
 public class App {
+    private static String getDatabaseUrl() {
+        return System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:webapp;DB_CLOSE_DELAY=-1;");
+    }
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
@@ -16,6 +21,12 @@ public class App {
     public static Javalin getApp() {
 
         // BEGIN
+        var hikariConfig = new HikariConfig();
+        String databaseUrl = getDatabaseUrl();
+        if (databaseUrl.contains("postgresql")) {
+            hikariConfig.setDriverClassName("org.postgresql.Driver");
+        }
+        hikariConfig.setJdbcUrl(databaseUrl);
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
